49b61a5ef376c4ea211b5fd617472085482b30b2
Alex Gulliver <randdalf@gmail.com>
2019-12-07 10:28:27 +0000

    Rewrtie Day 6, Puzzle 2 _again_ to use depth-first search

--- d06.py
+++ d06.py
@@ -24,29 +24,27 @@ def orbit_count_checksum(orbits, depth=0):
     return total_orbits(orbits, 'COM')
 
 
-def path_from_com(parents, body):
-    path = []
-    while body != 'COM':
-        path.append(body)
-        body = parents[body]
-    path.reverse()
-    return path
+def find_path(orbits, body, target, path):
+    if body == target:
+        return path
+
+    path.append(body)
+    for child in orbits[body]:
+        result = find_path(orbits, child, target, path)
+        if result:
+            return result
+    path.pop()
 
 
 def orbital_transfers(orbits):
-    parents = {}
-    for parent, children in orbits.items():
-        for child in children:
-            parents[child] = parent
-
-    you_path = path_from_com(parents, 'YOU')
-    san_path = path_from_com(parents, 'SAN')
+    you_path = find_path(orbits, 'COM', 'YOU', [])
+    san_path = find_path(orbits, 'COM', 'SAN', [])
 
     needle = 0
     while you_path[needle] == san_path[needle]:
         needle += 1
 
-    return len(san_path) + len(you_path) - 2 * (needle + 1)
+    return len(san_path) + len(you_path) - 2 * needle
 
 
 if __name__ == "__main__":
