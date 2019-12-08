7e487a50b5b0a24b96384ffa2d45376e4f08f401
Alex Gulliver <randdalf@gmail.com>
2019-12-08 09:43:48 +0000

    Day 8, Puzzle 2

--- d08.py
+++ d08.py
@@ -26,9 +26,25 @@ def fewest_zero_digits(digits, width, height):
     return result
 
 
+def decode_image(digits, width, height):
+    size = width * height
+    image = [2 for i in range(size)]
+    for start in range(0, len(digits), size):
+        for i, d in enumerate(digits[start:start+size]):
+            if image[i] == 2:
+                image[i] = d
+    pixels = [' # '[d] for d in image]
+    rows = [''.join(pixels[i:i+width]) for i in range(0, size, width)]
+    return '\n'.join(rows)
+
+
 def fewest_zero_digits_25_6(digits):
     return fewest_zero_digits(digits, 25, 6)
 
 
+def decode_image_25_6(digits):
+    return decode_image(digits, 25, 6)
+
+
 if __name__ == "__main__":
-    solve(8, parse, lambda ds: fewest_zero_digits_25_6(ds))
+    solve(8, parse, fewest_zero_digits_25_6, decode_image_25_6)
