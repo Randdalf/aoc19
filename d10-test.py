0e9444744821b22cc73031e40c38bf9aaee87af7
Alex Gulliver <randdalf@gmail.com>
2019-12-10 08:23:30 +0000

    Day 10, Puzzle 2

--- d10-test.py
+++ d10-test.py
@@ -6,6 +6,7 @@ import unittest
 
 from d10 import parse
 from d10 import best_location
+from d10 import vaporized_200
 
 example1 = parse("""......#.#.
 #..#.#....
@@ -74,6 +75,11 @@ class BestLocationTests(unittest.TestCase):
 
     def test_example4(slf):
         return slf.assertEqual(best_location(example4), 210)
+
+
+class Vaporized200Tests(unittest.TestCase):
+    def test_example4(slf):
+        return slf.assertEqual(vaporized_200(example4), 802)
 
 
 if __name__ == "__main__":
--- d10.py
+++ d10.py
@@ -2,6 +2,7 @@
 
 """Advent of Code 2019, Day 10"""
 
+from collections import defaultdict
 from math import atan2
 
 from aoc19 import solve
@@ -11,9 +12,6 @@ class Vec2:
     def __init__(slf, x, y):
         slf.x = x
         slf.y = y
-
-    def __add__(slf, otr):
-        return Vec2(slf.x + otr.x, slf.y + otr.y)
 
     def __sub__(slf, otr):
         return Vec2(slf.x - otr.x, slf.y - otr.y)
@@ -26,6 +24,12 @@ class Vec2:
 
     def __str__(slf):
         return f'({slf.x}, {slf.y})'
+
+    def __repr__(slf):
+        return f'Vec2({slf.x}, {slf.y})'
+
+    def sqlength(slf):
+        return slf.x * slf.x + slf.y * slf.y
 
 
 def parse(data):
@@ -40,18 +44,54 @@ def parse(data):
     return asteroids
 
 
+def angle(v):
+    return round(atan2(v.y, v.x), 3)
+
+
+def map_asteroids(asteroids, station):
+    mapped = defaultdict(list)
+    for asteroid in asteroids:
+        if station != asteroid:
+            offset = asteroid - station
+            mapped[angle(offset)].append(asteroid)
+    return mapped
+
+
 def best_location(asteroids):
-    max = None
-    for station in asteroids:
-        views = set()
-        for asteroid in asteroids:
-            if station != asteroid:
-                offset = asteroid - station
-                views.add(round(atan2(offset.y, offset.x), 4))
-        if max is None or len(views) > max:
-            max = len(views)
-    return max
+    return max(len(map_asteroids(asteroids, s).keys()) for s in asteroids)
+
+
+def vaporized_200(asteroids):
+    maps = {s: map_asteroids(asteroids, s) for s in asteroids}
+    station = max(maps.keys(), key=lambda x: len(maps[x].keys()))
+    world = list(maps[station].items())
+
+    # Sort the world by angle.
+    world.sort(key=lambda x: x[0])
+
+    # Sort each angle's asteroids by distance from the station. Reversed so we
+    # can pop from the end of each array to vaporize an asteroid.
+    for _, asteroids in world:
+        asteroids.sort(key=lambda x: (x - station).sqlength(), reverse=True)
+
+    # Find the index closest to the up angle.
+    up = angle(Vec2(0, -1))
+    index = 0
+    while world[index][0] < up:
+        index += 1
+
+    # Vaporize!
+    vaporized = []
+    while len(vaporized) < 200:
+        asteroids = world[index][1]
+        if len(asteroids) > 0:
+            vaporized.append(asteroids.pop())
+        index = (index + 1) % len(world)
+
+    # Return the 200th vaporized asteroid.
+    coord = vaporized[-1]
+    return coord.x * 100 + coord.y
 
 
 if __name__ == "__main__":
-    solve(10, parse, best_location)
+    solve(10, parse, best_location, vaporized_200)
