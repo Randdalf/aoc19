9d473340e1d97f915106abd5613a3e96f6b50e75
Alex Gulliver <randdalf@gmail.com>
2019-12-17 18:50:00 +0000

    Day 17, Puzzle 2

--- d17.py
+++ d17.py
@@ -2,8 +2,10 @@
 
 """Advent of Code 2019, Day 17"""
 
+from itertools import product, repeat
+
 from aoc19 import solve
-from intcode import intcode
+from intcode import intcode, IntcodeCPU
 from vec2 import Vec2
 
 
@@ -14,8 +16,18 @@ class TILE:
     RIGHT = '>'
     UP = '^'
     DOWN = 'v'
-    TUMBLING = 'X'
+
+
+class INPUT:
+    LEFT = 'L'
+    RIGHT = 'R'
+    A = 'A'
+    B = 'B'
+    C = 'C'
+    COMMA = ','
     NEWLINE = '\n'
+    YES = 'y'
+    NO = 'n'
 
 
 dirs = {
@@ -25,17 +37,44 @@ dirs = {
     TILE.DOWN: Vec2(0, +1)
 }
 
+order = [
+    Vec2(0, -1),
+    Vec2(1, 0),
+    Vec2(0, 1),
+    Vec2(-1, 0)
+]
+
 
 def parse(data):
     return list(map(int, data.split(',')))
 
 
-def alignment_parameters(program, display=False):
+def select_dir(pos, view, visited):
+    for dir in dirs.values():
+        next = pos + dir
+        if next not in visited and next in view:
+            if view[next] == TILE.SCAFFOLD:
+                return dir
+
+
+def rotate(from_dir, to_dir):
+    n = len(order)
+    from_index = order.index(from_dir)
+    to_index = order.index(to_dir)
+    right_dist = (to_index - from_index) % n
+    left_dist = (from_index - to_index + n) % n
+    if left_dist < right_dist:
+        return repeat(INPUT.LEFT, left_dist)
+    else:
+        return repeat(INPUT.RIGHT, right_dist)
+
+
+def find_path(program, display=False):
     x = 0
     y = 0
     view = {}
     for tile in map(chr, intcode(program).outputs):
-        if tile == TILE.NEWLINE:
+        if tile == '\n':
             x = 0
             y += 1
         else:
@@ -43,6 +82,7 @@ def alignment_parameters(program, display=False):
             view[pos] = tile
             if tile in dirs.keys():
                 start = pos
+                facing = dirs[tile]
                 dir = tile
             x += 1
 
@@ -55,28 +95,95 @@ def alignment_parameters(program, display=False):
                 row += view[Vec2(x, y)]
             print(row)
 
-    def select_dir(pos, view, visited):
-        for dir in dirs.values():
-            next = pos + dir
-            if next not in visited and next in view:
-                if view[next] == TILE.SCAFFOLD:
-                    return dir
-
     visited = set()
-    intersections = set()
+    path = []
+    moves = []
     pos = start
     dir = select_dir(pos, view, visited)
     while dir:
-        while (pos + dir) in view and view[(pos + dir)] == TILE.SCAFFOLD:
-            if pos in visited:
-                intersections.add(pos)
-            else:
-                visited.add(pos)
-            pos += dir
+        next = pos + dir
+        moves.extend(rotate(facing, dir))
+        facing = dir
+        n = 0
+        while next in view and view[next] == TILE.SCAFFOLD:
+            visited.add(pos)
+            path.append(pos)
+            pos = next
+            next += dir
+            n += 1
+        moves.append(str(n))
         dir = select_dir(pos, view, visited)
 
+    return path, moves
+
+
+def alignment_parameters(program):
+    path, moves = find_path(program)
+    visited = set()
+    intersections = set()
+    for pos in path:
+        if pos in visited:
+            intersections.add(pos)
+        else:
+            visited.add(pos)
     return sum(p.x * p.y for p in intersections)
 
 
+def space_dust(program):
+    path, moves = find_path(program)
+
+    for an, bn, cn in product(range(1, 11), range(1, 11), range(1, 11)):
+        main = []
+        offset = 0
+
+        # Select A and find its initial repetitions.
+        a = moves[offset:offset+an]
+        while offset < len(moves):
+            if moves[offset:offset+an] == a:
+                main.append(INPUT.A)
+                offset += an
+            else:
+                break
+
+        # Select B and find repetitions of A and B.
+        b = moves[offset:offset+bn]
+        while offset < len(moves):
+            if moves[offset:offset+an] == a:
+                main.append(INPUT.A)
+                offset += an
+            elif moves[offset:offset+bn] == b:
+                main.append(INPUT.B)
+                offset += bn
+            else:
+                break
+
+        # Select C and find repetitions of A, B and C.
+        c = moves[offset:offset+cn]
+        while offset < len(moves):
+            if moves[offset:offset+an] == a:
+                main.append(INPUT.A)
+                offset += an
+            elif moves[offset:offset+bn] == b:
+                main.append(INPUT.B)
+                offset += bn
+            elif moves[offset:offset+cn] == c:
+                main.append(INPUT.C)
+                offset += cn
+            else:
+                break
+
+        # We found our functions if we perfectly match the move list.
+        if offset == len(moves):
+            input = ','.join(main) + '\n'
+            input += ','.join(a) + '\n'
+            input += ','.join(b) + '\n'
+            input += ','.join(c) + '\n'
+            input += INPUT.NO + '\n'
+            robot = IntcodeCPU(program, *list(map(ord, input)))
+            robot.memory[0] = 2
+            robot.execute()
+            return robot.outputs[-1]
+
+
 if __name__ == "__main__":
-    solve(17, parse, alignment_parameters)
+    solve(17, parse, alignment_parameters, space_dust)
