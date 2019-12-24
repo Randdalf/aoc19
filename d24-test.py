f6a155fa696d46f306fe4acad1e48cf9abee39b4
Alex Gulliver <randdalf@gmail.com>
2019-12-24 10:03:19 +0000

    Day 24, Puzzle 2

--- d24-test.py
+++ d24-test.py
@@ -4,7 +4,7 @@
 
 import unittest
 
-from d24 import parse, biodiversity_rating
+from d24 import parse, biodiversity_rating, recursive_bugs
 
 example1 = """....#
 #..#.
@@ -16,6 +16,11 @@ example1 = """....#
 class BiodiversityRatingTests(unittest.TestCase):
     def test_example1(slf):
         slf.assertEqual(biodiversity_rating(parse(example1)), 2129920)
+
+
+class RecursiveBugsTests(unittest.TestCase):
+    def test_example1(slf):
+        slf.assertEqual(recursive_bugs(parse(example1), minutes=10), 99)
 
 
 if __name__ == "__main__":
--- d24.py
+++ d24.py
@@ -2,7 +2,7 @@
 
 """Advent of Code 2019, Day 24"""
 
-from collections import defaultdict
+from collections import defaultdict, deque
 from itertools import product
 
 from aoc19 import solve
@@ -11,6 +11,7 @@ from aoc19 import solve
 class TILE:
     BUG = '#'
     EMPTY = '.'
+    RECURSIVE = '?'
 
 
 def parse(data):
@@ -56,7 +57,7 @@ def biodiversity_rating(grid, display=False):
             tile = grid[(x, y)]
             if tile == TILE.BUG and bugs != 1:
                 new_grid[(x, y)] = TILE.EMPTY
-            elif tile == TILE.EMPTY and bugs >= 1 and bugs <= 2:
+            elif tile == TILE.EMPTY and 1 <= bugs <= 2:
                 new_grid[(x, y)] = TILE.BUG
             else:
                 new_grid[(x, y)] = tile
@@ -76,5 +77,104 @@ def biodiversity_rating(grid, display=False):
     return rating
 
 
+def adjacencies(x, y, level):
+    # North
+    north = y - 1
+    if north == -1:
+        yield (2, 1, level-1)
+    elif x == 2 and north == 2:
+        for ax in range(5):
+            yield (ax, 4, level+1)
+    else:
+        yield (x, north, level)
+
+    # South
+    south = y + 1
+    if south == 5:
+        yield (2, 3, level-1)
+    elif x == 2 and south == 2:
+        for ax in range(5):
+            yield (ax, 0, level+1)
+    else:
+        yield (x, south, level)
+
+    # West
+    west = x - 1
+    if west == -1:
+        yield (1, 2, level-1)
+    elif west == 2 and y == 2:
+        for ay in range(5):
+            yield (4, ay, level+1)
+    else:
+        yield (west, y, level)
+
+    # East
+    east = x + 1
+    if east == 5:
+        yield (3, 2, level-1)
+    elif east == 2 and y == 2:
+        for ay in range(5):
+            yield (0, ay, level+1)
+    else:
+        yield (east, y, level)
+
+
+def print_irg(irg):
+    min_depth = min(level for x, y, level in irg.keys())
+    max_depth = max(level for x, y, level in irg.keys())
+
+    for depth in range(min_depth, max_depth+1):
+        if depth == min_depth:
+            print(f'Depth {depth}:')
+        else:
+            print(f'\nDepth {depth}:')
+        for y in range(5):
+            row = ''
+            for x in range(5):
+                if x == 2 and y == 2:
+                    row += '?'
+                else:
+                    row += irg[(x, y, depth)]
+            print(row)
+
+
+def recursive_bugs(grid, minutes=200, display=False):
+    irg = defaultdict(
+        lambda: TILE.EMPTY,
+        {(x, y, 0): tile for (x, y), tile in grid.items()}
+    )
+    irg[(2, 2, 0)] = TILE.RECURSIVE
+
+    for minute in range(minutes):
+        new_irg = defaultdict(lambda: TILE.EMPTY)
+
+        # Add all bug tiles, and tiles adjacent to bug tiles, for processing.
+        pending = set()
+        for key, tile in irg.items():
+            if tile == TILE.BUG:
+                pending.add(key)
+                pending.update(adjacencies(*key))
+
+        # Process all the interesting tiles.
+        while len(pending) > 0:
+            key = pending.pop()
+            tile = irg[key]
+            bugs = sum(int(irg[adj] == TILE.BUG) for adj in adjacencies(*key))
+            if tile == TILE.BUG and bugs != 1:
+                new_irg[key] = TILE.EMPTY
+            elif tile == TILE.EMPTY and 1 <= bugs <= 2:
+                new_irg[key] = TILE.BUG
+            else:
+                new_irg[key] = tile
+
+        # Switch to the updated grid.
+        irg = new_irg
+
+    if display:
+        print_irg(irg)
+
+    return list(irg.values()).count(TILE.BUG)
+
+
 if __name__ == "__main__":
-    solve(24, parse, biodiversity_rating)
+    solve(24, parse, biodiversity_rating, recursive_bugs)
